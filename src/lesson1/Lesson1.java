3edebf3668129dad71a09aba45e2f8dd62c65b30
Jane <Jane@mail.ru>
2023-10-01 22:51:34 +0300

    первый коммит

---
+++ src/lesson1/Lesson1.java
@@ -0,0 +1,10 @@
+package lesson1;
+
+public class Lesson1 {
+    //psvm + Enter
+    public static void main(String[] args) {
+        // sout +Enter
+        System.out.println("Lesson1");
+
+    }
+}
