93e7496362d970a3957ddec8a7827a527276d71b
Jane <Jane@mail.ru>
2023-10-13 16:49:52 +0300

    дз6_2

--- dz6/src/Dz6/Dz0601.java
+++ dz6/src/Dz6/Dz0601.java
@@ -4,11 +4,13 @@ import java.util.Scanner;
 
 public class Dz0601 {
     public static void main(String[] args) {
-            Scanner scanner = new Scanner(System.in);
-            String string = scanner.next();
+        String string1 = "level";
+        String string2 = "hvsdjvh";
 
-            String revers = new StringBuilder(string).reverse().toString();
+        String revers1 = new StringBuilder(string1).reverse().toString();
+        String revers2 = new StringBuilder(string2).reverse().toString();
 
-            System.out.println(string.equalsIgnoreCase(revers));
+        System.out.println(string1.equalsIgnoreCase(revers1));
+        System.out.println(string2.equalsIgnoreCase(revers2));
     }
 }
